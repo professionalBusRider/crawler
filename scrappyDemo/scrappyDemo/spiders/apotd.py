c9b975b72234c570563de49fa4be8aa230a433c2
professionalBusRider <professionalBusRider@gmail.com>
2015-04-08 16:17:02 -0500

    more

---
+++ scrappyDemo/scrappyDemo/spiders/apotd.py
@@ -0,0 +1,17 @@
+import scrapy
+from scrappyDemo.items import Website
+
+class aptod(scrapy.Spider):
+    name = "aptod"
+    allowed_domains = ["apod.nasa.gov"]
+    start_urls = [ "http://apod.nasa.gov/apod/astropix.html" ]
+
+    def parse(self, response): #parses response data
+
+        websiteItem = Website()
+        selection = response.xpath('/html/body/center[2]/b[1]')
+
+        websiteItem['name'] = "APOTD Title"
+        websiteItem['description'] = selection.xpath('text()').extract()
+        websiteItem['url'] = response
+        yield websiteItem
